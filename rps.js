ab2cfcab0cdfee6f5b3128bfc858a08e2fd0739e
ttreit <14933277+ttreit@users.noreply.github.com>
2019-04-21 15:24:40 -0700

    Add pseudocode for Rock Paper Scissors game.

---
+++ rps.js
@@ -0,0 +1,12 @@
+// Create a simple rock, paper, scissors game in java script
+
+//Add Welcome messaage
+//1.1 Ask user how many games they'd like to play (best 3 out of 5 etc)
+//Ask user to select rock, paper, or scissors
+//Show them their selection
+//Have computer randomly select rock, paper, or scissors
+//Display computer selection
+//Determine outcome
+    //paper beats rock, rock beats scissors, scissors beats paper
+//Display win, lose, tie message
+//Ask if user wants to play again
