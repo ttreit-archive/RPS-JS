f6916c9e88bbe03b07f037e7adf424ba39ff9444
ttreit <14933277+ttreit@users.noreply.github.com>
2019-04-22 14:20:41 -0700

    Add user instructions

--- rps.js
+++ rps.js
@@ -1,8 +1,31 @@
 // Create a simple rock, paper, scissors game in java script
 
 //Add Welcome messaage
+//Added in rps.html
+
 //1.1 Ask user how many games they'd like to play (best 3 out of 5 etc)
+
 //Ask user to select rock, paper, or scissors
+
+document.write("Select your weapon! <br> <br>");
+
+// Add buttons here:
+
+/* 
+
+<button class="favorite styled"
+        type="button">
+    Add to favorites
+</button> 
+
+*/
+
+
+//Press 1 for Rock <br>Press 2 for Paper<br>Press 3 for Scissors");
+
+//Get the input from the user
+
+
 //Show them their selection
 //Have computer randomly select rock, paper, or scissors
 //Display computer selection
